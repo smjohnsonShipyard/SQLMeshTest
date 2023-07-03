3327377f29529285f2dc039a3ad19de61b88748d
smjohnsonShipyard <104784108+smjohnsonShipyard@users.noreply.github.com>
2023-07-03 14:13:32 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+from sqlmesh import SQLMesh
+
+# Create an instance of the SQLMesh class
+mesh = SQLMesh(config_path='config.yaml')
+
+# Define the model
+model_name = 'full_model'
+
+# Run the model
+mesh.run_model(model_name)
